3749224f3ddeff6d798258804ed906793312b732
Evlav <varotpava@gmail.com>
2023-05-16 16:32:22 -0700

    literally only added a single font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,180 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import * as React from 'react';
+import { useState, useRef } from 'react';
+import Weather from "./Weather.js";
+import "@fontsource/inter";
+
+
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
+);
+
+
+function App(){
+  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const [inputValue, setInputValue] = useState("");
+  const [units, setUnits] = useState("metric");
+  const [unitdisplay, setUnitsDis] = useState("Celcius");
+  const [degree, setDegree] = useState("°C")
+  const [city, setCity] = useState("London");
+
+  //Rearrange list items
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+  };
+
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+  };
+
+  const drop = (e) => {
+    const copyListItems = [...task];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setTask(copyListItems);
+  };
+
+  //init list
+  const [task, setTask] = useState([
+    { taskName: 'Task 1', taskEditable: false },
+    { taskName: 'Task 2', taskEditable: false },
+    { taskName: 'Task 3', taskEditable: false }
+  ]);
+
+  //new button
+  function handleNewItem(){
+    const newTask = { taskName : 'Untitled', taskEditable: true };
+    const newTasks = [...task, newTask];
+    setTask(newTasks);
+  }
+
+  //handle click on task
+  function handleTaskClick(index){
+    const newTasks = [...task];
+    const allTasksEditable = task.every((task) => task.taskEditable === false);
+    
+    if (allTasksEditable || task[index].taskEditable === true){
+      newTasks[index].taskEditable = true;
+      
+    }else{
+      
+      newTasks[index].taskEditable = false;
+      
+    }
+
+    setTask(newTasks);
+  }
+
+  //handle rename
+  function handleTaskNameChange(index, newName){
+    const newTasks = [...task];
+    newTasks[index].taskName = newName;
+    newTasks[index].taskEditable = false;
+    setTask(newTasks);
+    setInputValue("");
+  }
+
+  //handle input location{
+ 
+    const handleCityChange = (e) => {
+      setCity(prompt("Input City"));
+    };
+
+    function handleUnitChange(){
+      console.log(degree)
+      if (units === "metric"){
+        setUnits("imperial");
+        setUnitsDis("Farenheit");
+        setDegree("°F");
+      }
+      else{
+        setUnits("metric");
+        setUnitsDis("Celcius");
+        setDegree("°C");
+      }
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container>
+        <Grid item md={1.7} className={days[1]} key={days[1].id}>
+          <Typography className='DayContainer'>{days[0]}</Typography>
+          {task.map((task, index) => (
+            <Paper draggable onClick={() => handleTaskClick(index)}
+            onDragStart={(e) => dragStart(e, index)}
+            onDragEnter={(e) => dragEnter(e, index)}
+            onDragEnd={drop} key = {index.toString()}>
+              {task.taskEditable ? (
+                <input
+                  type="text"
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  onKeyDown={(e) => {if (e.key === "Enter")
+                                      handleTaskNameChange(index, inputValue)}}
+                  
+                />
+              ) : (
+                <span>{task.taskName}</span>
+              )}
+            </Paper>
+          ))}
+          <Paper onClick={() => handleNewItem()}>Add Task</Paper>
+          <Weather city={city} units={units} degree={degree}/>
+        </Grid>
+      </Grid>
+      <Stack direction="row" spacing={2}>
+        <Button variant="outlined">Add Weekly Task</Button>
+        <Button variant="outlined">Add Monthly Task</Button>
+        <Button variant="outlined">Add Recurring Task</Button>
+        <Button variant="outlined" onClick={() => handleUnitChange(units)}>{unitdisplay}</Button>
+        <Button variant="outlined" onClick={handleCityChange}>Select Location</Button>
+      </Stack>
     </div>
   );
-}
+};
 
 export default App;
+
+/* for duplicate over a week
+return (
+    <div className="App">
+      <Grid container>
+          {days.map((day) => 
+            <Grid item md={1.7} className={day} key={day.id}>
+              <Paper>{day}</Paper>
+
+              {task.map((task) => <Paper>{task.taskName}</Paper>)}
+
+              <Button variant="outlined" key={day.id}
+                onClick={() => handleNewItem()}
+                >Add Task
+              </Button>
+             </Grid>)
+          }
+        
+      </Grid>
+    </div>
+  );
+  */
