5d445953bc2d77465a47343c6d4eba43f9dc8ed8
Evlav <varotpava@gmail.com>
2023-05-17 16:02:40 -0700

    updated gitignore

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Alert from '@mui/material/Alert';
-import { API_KEY } from './config.js';
+import { API_KEY } from '../config.js';
 
 
 function Weather(props) {
